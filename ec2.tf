0463f231eeb897b3e87d3f9fed7684ae68a65017
rizk <arezk95@gmail.com>
2022-08-23 20:00:47 +0200

    update

--- ec2.tf
+++ ec2.tf
@@ -10,14 +10,3 @@ resource "aws_instance" "bastionHost" {
     Name = "bastionHost"
   }
 }
-
-resource "aws_instance" "privateinstance" {
-  ami                    = var.instances_AMI
-  instance_type          = var.instance_type
-  subnet_id              = module.my_network.private1_subnet_id
-  vpc_security_group_ids = [aws_security_group.allow_3000.id]
-  key_name               = aws_key_pair.generated_key.key_name #attach the public key i created and saved its private one in secrets manager
-  tags = {
-    Name = "private-instance"
-  }
-}
