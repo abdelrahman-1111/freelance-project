7c615a873e1b6d4d54c27176bdd7657e60099130
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-08-23 19:30:55 +0200

    modify the backend

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
 
     backend "s3" {
-        bucket = "iti-course-terraformstate"
+        bucket = "terraform-state"
         key    = "terraform.tfstate"
         region = "us-east-1"
     }
