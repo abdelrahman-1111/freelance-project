8945fa55d3f7b3128430c342cb2d420529d8389b
rizk <arezk95@gmail.com>
2022-08-23 20:17:59 +0200

    update ec2

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "bastionHost" {
   ami                         = var.instances_AMI
   instance_type               = var.instance_type
   subnet_id                   = module.my_network.public1_subnet_id
-  key_name                    = aws_key_pair.generated_key.key_name #attach the public key i created and saved its private one in secrets manager
   associate_public_ip_address = true                                #to provide it with public ip 
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   tags = {
