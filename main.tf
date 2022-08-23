1a27b751b98cd352b79a619d94c18a7173741f78
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-08-23 19:49:49 +0200

    create the main

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "my_network" {
+    source               = "./network"
+    vpc_region           = var.region
+    vpc_CIDR             = var.vpc_CIDR
+    public_subnet1_CIDR  = var.public_subnet1_CIDR
+    public_subnet2_CIDR  = var.public_subnet2_CIDR
+    private_subnet1_CIDR = var.private_subnet1_CIDR
+    private_subnet2_CIDR = var.private_subnet2_CIDR
+}
